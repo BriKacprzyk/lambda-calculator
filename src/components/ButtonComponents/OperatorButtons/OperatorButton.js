1d4161a0f911bb7b934b9e4eb51161e36c4019ab
Bridget Kacprzyk <bridgetkacprzyk174@gmail.com>
2019-10-14 16:50:03 -0400

    Finished the project but cant get the buttons to work

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const OperatorButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const OperatorButton = props => {
+    return (
+        <button className="button operator">
+            {/* Display a button element rendering the data being passed down from the parent container on props */}
+            {props.operatorData}
+        </button>
+    );
 };
+
+export default OperatorButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
+
 import React from "react";
 
-const SpecialButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const SpecialButton = props => {
+    return (
+        <button className="button special">
+            {/* Display a button element rendering the data being passed down from the parent container on props */}
+            {props.specialData}
+        </button>
+    );
 };
+
+export default SpecialButton;
